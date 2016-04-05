30c7478f66eca8e1ee67ceeec4fbe7a82c54e86f
Nolan Foster <nolanfoster@gmail.com>
2016-04-05 16:16:58 -0400

    added data table

--- io/gpio.lua
+++ io/gpio.lua
@@ -1,16 +1,22 @@
 return function(gpio, type)
-  pin = gpio
-  status, temp, humi, temp_dec, humi_dec = dht.read(pin)
-  if status == dht.OK then
-      print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
-            math.floor(temp),
-            temp_dec,
-            math.floor(humi),
-            humi_dec
-      ))
-  elseif status == dht.ERROR_CHECKSUM then
-      print( "DHT Checksum error." )
-  elseif status == dht.ERROR_TIMEOUT then
-      print( "DHT timed out." )
+  data = {}
+  if type = "dht" then
+    data.status, data.temp, data.humi, data.temp_dec, data.humi_dec = dht.read(gpio)
+    if status == dht.OK then
+      --Since we are using integer firmware we need to concatinate the values
+          data.temp = math.floor(data.temp)
+          data.humi = math.floor(data.humi)
+        print(string.format("DHT Temperature:%d.%03d;Humidity:%d.%03d\r\n",
+              data.temp,
+              data.temp_dec,
+              data.humi,
+              data.humi_dec
+        ))
+    elseif status == dht.ERROR_CHECKSUM then
+        data.status = "DHT Checksum error."
+    elseif status == dht.ERROR_TIMEOUT then
+        data.status = "DHT timed out." )
+    end
+    return data
   end
 end
